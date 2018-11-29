697c11af0e3ab9a6e1a569ac05bf1c53cd8e80b4
antoniocaceres <acaceres@student.mamkschools.org>
2018-11-29 09:12:04 -0500

    Chapter 8

---
+++ src/CH8Classes/PointClientV1.java
@@ -0,0 +1,13 @@
+package CH8Classes;
+
+public class PointClientV1 {
+    public static void main (String args[]) {
+        //instantiate a point object called p1
+        PointV1 p1 = new PointV1();
+        System.out.println("p1: (" + p1.x + ", " + p1.y + ")");
+        p1.x =3;
+        p1.x = 6;
+        System.out.println("p1': (" + p1.x + ", " + p1.y + ")");
+        //PointV1 p2 = new PointV1(7,25);
+    }
+}
---
+++ src/CH8Classes/PointV1.java
@@ -0,0 +1,17 @@
+package CH8Classes;
+// This class is a template to represent
+// a point on the cartesian coordinate
+public class PointV1 {
+    //state fields below
+    public int x;
+    public int y;
+    //behaviors below (methods)
+    public void translate(int x, int y){
+
+
+
+    }
+    public void setLocation(int x, int y) {
+
+    }
+}
